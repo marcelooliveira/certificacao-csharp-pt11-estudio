694a3a9aeff3cff69dfefe50f4528f27ef1ebd34
marcelooliveira <mclricardo@gmail.com>
2019-02-13 14:51:17 -0200

    .

--- Program01.04/Program.cs
+++ Program01.04/Program.cs
@@ -9,13 +9,6 @@ namespace Program01._04
 {
     class Program
     {
-        static void WorkOnItem(object item)
-        {
-            Console.WriteLine("Começando a trabalhar com: " + item);
-            Thread.Sleep(100);
-            Console.WriteLine("Terminando a trabalhar com: " + item);
-        }
-
         static void Main(string[] args)
         {
             var items = Enumerable.Range(0, 100).ToArray();
@@ -25,12 +18,19 @@ namespace Program01._04
                 if (i == 75)
                     loopState.Stop();
 
-                WorkOnItem(items[i]);
+                Processar(items[i]);
             });
             Console.WriteLine("Completado? " + result.IsCompleted);
             Console.WriteLine("No. de itens processados: " + result.LowestBreakIteration);
             Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
             Console.ReadLine();
+        }
+
+        static void Processar(object item)
+        {
+            Console.WriteLine("Começando a trabalhar com: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Terminando a trabalhar com: " + item);
         }
     }
 }
