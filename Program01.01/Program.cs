686efa149317b14fd6d758edead8a8db0c7c7965
marcelooliveira <mclricardo@gmail.com>
2019-02-14 14:28:05 -0200

    ..

--- Program01.01/Program.cs
+++ Program01.01/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Program01
 {
@@ -11,19 +8,19 @@ namespace Program01
         static void Main(string[] args)
         {
             //TAREFA 1: Cozinhar e refogar EM SÉRIE
-
             //TAREFA 2: Cozinhar e refogar EM PARALELO
-
             //TAREFA 3: Medir o tempo dos 2 procedimentos
 
-            Console.WriteLine("Retire do fogo e ponha o molho sobre o macarrão. Bom apetite! Tecle [ENTER] para terminar.");
+            Console.WriteLine(
+                "Retire do fogo e ponha o molho sobre o macarrão. " +
+                "Bom apetite! " +
+                "Tecle [ENTER] para terminar.");
             Console.ReadLine();
         }
 
         static void CozinharMacarrao()
         {
             Console.WriteLine("Cozinhando macarrão...");
-            Thread.Sleep(1000);
             Console.WriteLine("Macarrão já está cozido!");
             Console.WriteLine();
         }
@@ -31,7 +28,6 @@ namespace Program01
         static void RefogarMolho()
         {
             Console.WriteLine("Refogando molho...");
-            Thread.Sleep(2000);
             Console.WriteLine("Molho já está refogado!");
             Console.WriteLine();
         }
