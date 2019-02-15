ba30ffa39c00f7bc086fdd9d200f341d1cd7df7a
marcelooliveira <mclricardo@gmail.com>
2019-02-15 13:44:14 -0200

    .

--- Program01.04/Program.cs
+++ Program01.04/Program.cs
@@ -1,143 +1,210 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 namespace Program01
 {
     class Program
     {
-        class Pessoa : IComparable
+        public class Filme
         {
-            public string Nome { get; set; }
-            public string Cidade { get; set; }
+            public string Titulo { get; set; }
+            public decimal Faturamento { get; set; }
+            public decimal Orcamento { get; set; }
+            public string Distribuidor { get; set; }
+            public string Genero { get; set; }
+            public string Diretor { get; set; }
+        }
+
+        class ItemResumo : IComparable
+        {
+            public string Item { get; set; }
+            public decimal Faturamento { get; set; }
+            public decimal Orcamento { get; set; }
 
             public int CompareTo(object obj)
             {
-                var outro = obj as Pessoa;
-                return this.Nome.CompareTo(outro.Nome);
+                ItemResumo outro = obj as ItemResumo;
+                return Item.CompareTo(outro.Item);
             }
         }
 
         static void Main(string[] args)
         {
-            Pessoa[] pessoas = new Pessoa[] {
-                new Pessoa { Nome = "Daniel", Cidade = "Aracaju" },
-                new Pessoa { Nome = "Carlos", Cidade ="Londrina" },
-                new Pessoa { Nome = "Tiago", Cidade = "Londrina" },
-                new Pessoa { Nome = "Fabio", Cidade = "Uberlândia" },
-                new Pessoa { Nome = "Breno", Cidade = "Aracaju" },
-                new Pessoa { Nome = "Alice", Cidade = "Uberaba" },
-                new Pessoa { Nome = "Guilherme", Cidade = "Uberaba" },
-                new Pessoa { Nome = "Isaias", Cidade = "Aracaju" },
-                new Pessoa { Nome = "Henrique", Cidade = "Aracaju" },
-                new Pessoa { Nome = "Eduardo", Cidade = "Aparecida" }
+
+            List<Filme> filmes = JsonConvert.DeserializeObject<List<Filme>>(File.ReadAllText("filmes.json"));
+
+            //Tarefa 1: obter a lista de filmes de Aventura 
+            var resultado1 =
+            from filme in filmes
+            where filme.Genero == "Adventure"
+            select new ItemResumo
+            {
+                Item = filme.Titulo,
+                Faturamento = filme.Faturamento,
+                Orcamento = filme.Orcamento
             };
-                       
-            //Tarefa 1: obter a lista de pessoas de Aracaju 
+            GeraRelatorio("Tarefa 1: obter a lista de filmes de Aventura ", resultado1);
 
-            var resultado1 = 
-                         from pessoa in pessoas
-                         where pessoa.Cidade == "Aracaju"
-                         select pessoa;
-
-            Imprimir(resultado1);
-
-            //Tarefa 2: obter a lista de pessoas de Aracaju,
+            //Tarefa 2: obter a lista de filmes de Aventura,
             //executando em PARALELO
+            var resultado2 =
+            from filme in filmes.AsParallel()
+            where filme.Genero == "Adventure"
+            select new ItemResumo
+            {
+                Item = filme.Titulo,
+                Faturamento = filme.Faturamento,
+                Orcamento = filme.Orcamento
+            };
 
-            var resultado2 = 
-                         from pessoa in pessoas.AsParallel()
-                         where pessoa.Cidade == "Aracaju"
-                         select pessoa;
+            GeraRelatorio("Tarefa 2: obter a lista de filmes de Aventura, executando em PARALELO", 
+                resultado2);
 
-            Imprimir(resultado2);
+            //Tarefa 3: obter a lista de filmes de Aventura,
+            //executando em PARALELO com modo de execução default
 
             var resultado3 =
-             from pessoa in pessoas
+             from filme in filmes
                 .AsParallel()
                 .WithExecutionMode(ParallelExecutionMode.Default)
-             where pessoa.Cidade == "Aracaju"
-             select pessoa;
+             where filme.Genero == "Adventure"
+             select new ItemResumo
+             {
+                 Item = filme.Titulo,
+                 Faturamento = filme.Faturamento,
+                 Orcamento = filme.Orcamento
+             };
 
-            Imprimir(resultado3);
+            GeraRelatorio("Tarefa 3: obter a lista de filmes de Aventura, executando em PARALELO com modo de execução default",
+                resultado3);
 
+            //Tarefa 4: obter a lista de filmes de Aventura,
+            //executando em PARALELO forçando paralelismo
 
             var resultado4 =
-             from pessoa in pessoas
+             from filme in filmes
                 .AsParallel()
                 .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
-             where pessoa.Cidade == "Aracaju"
-             select pessoa;
+             where filme.Genero == "Adventure"
+             select new ItemResumo
+             {
+                 Item = filme.Titulo,
+                 Faturamento = filme.Faturamento,
+                 Orcamento = filme.Orcamento
+             };
 
-            Imprimir(resultado4);
+            GeraRelatorio("obter a lista de filmes de Aventura, executando em PARALELO forçando paralelismo", 
+                resultado4);
 
-            //var result = from pessoa in
-            //    pessoas
-            //    .AsParallel()
-            //    .WithDegreeOfParallelism(4)
-            //    .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
-            //             where pessoa.Cidade == "Aracaju"
-            //             select pessoa;
+            //Tarefa 5: obter a lista de filmes de Aventura,
+            //executando em PARALELO forçando paralelismo
+            //e com grau de paralelismo = 4
 
-            //var result = from pessoa in
-            //                 pessoas.AsParallel().AsOrdered()
-            //             where pessoa.Cidade == "Aracaju"
-            //             select pessoa;
+            var resultado5 =
+             from filme in filmes
+                .AsParallel()
+                .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                .WithDegreeOfParallelism(4)
+             where filme.Genero == "Adventure"
+             select new ItemResumo
+             {
+                 Item = filme.Titulo,
+                 Faturamento = filme.Faturamento,
+                 Orcamento = filme.Orcamento
+             };
 
-            //var result = (from pessoa in
-            //     pessoas.AsParallel()
-            //              where pessoa.Cidade == "Aracaju"
-            //              orderby (pessoa.Nome)
-            //              select new
-            //              {
-            //                  pessoa.Nome
-            //              }).AsSequential().Take(4);
+            GeraRelatorio("obter a lista de filmes de Aventura, executando em PARALELO forçando paralelismo e com grau de paralelismo = 4", resultado5);
+            
+            //Tarefa 6: obter a lista de filmes de Aventura,
+            //executando em PARALELO e preservando a ordem
 
-            //var result = from pessoa in
-            //                pessoas.AsParallel()
-            //             where pessoa.Cidade == "Aracaju"
-            //             select pessoa;
-            //result.ForAll(pessoa => Console.WriteLine(pessoa.Nome));
+            var resultado6 = 
+                from filme in filmes
+                    .AsParallel()
+                    .AsOrdered()
+                where filme.Genero == "Adventure"
+                select new ItemResumo
+                {
+                    Item = filme.Titulo,
+                    Faturamento = filme.Faturamento,
+                    Orcamento = filme.Orcamento
+                };
+
+            GeraRelatorio("Tarefa 6: obter a lista de filmes de Aventura, executando em PARALELO e preservando a ordem", 
+                resultado6);
 
 
+            //Tarefa 7: obter os 4 filmes de Aventura de maior faturamento,
+            //executando em PARALELO
 
-            //try
-            //{
-            //    var result = from pessoa in
-            //                     pessoas.AsParallel()
-            //                  where VerificaCidade(pessoa.Cidade)
-            //                  select pessoa;
+            var resultado7 =
+                (from filme in filmes
+                    .AsParallel()
+                orderby filme.Faturamento descending
+                where filme.Genero == "Adventure"
+                select new ItemResumo
+                {
+                    Item = filme.Titulo,
+                    Faturamento = filme.Faturamento,
+                    Orcamento = filme.Orcamento
+                }).AsSequential().Take(4);
 
-            //    result.ForAll(pessoa => Console.WriteLine(pessoa.Nome));
-            //}
-            //catch (AggregateException e)
-            //{
-            //    Console.WriteLine(e.InnerExceptions.Count + " exceções.");
-            //}
+            GeraRelatorio("Tarefa 7: obter os 4 filmes de Aventura de maior faturamento, executando em PARALELO", resultado7);
 
+
+            //Tarefa 8: Imprimir somente os títulos dos filmes,
+            //de aventura, consultando em PARALELO e usando uma
+            //ação em PARALELO
+
+            var resultado8 =
+                         from filme in
+                            filmes
+                            .AsParallel()
+                         where filme.Genero == "Adventure"
+                         select filme;
+
+            resultado8.ForAll(filme => Console.WriteLine(filme.Titulo));
 
             Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
             Console.ReadLine();
         }
 
-        private static void Imprimir(IEnumerable<Pessoa> result)
+        private static void GeraRelatorio(string tituloRelatorio, IEnumerable<ItemResumo> result)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            foreach (var pessoa in result)
-                Console.WriteLine(pessoa.Nome);
-            stopwatch.Stop();
-            Console.WriteLine("Tempo decorrido: {0} segundos", stopwatch.ElapsedMilliseconds / 1000.0);
-        }
+            Console.WriteLine("Relatório: {0}", tituloRelatorio);
 
-        public static bool VerificaCidade(string nome)
-        {
-            if (nome== "")
+            Console.WriteLine("{0,-30} {1,20:N2} {2,20:N2} {3,20:N2} {4,10:P}",
+                    "Item",
+                    "Faturamento",
+                    "Orcamento",
+                    "Lucro",
+                    "LucroPorcentagem");
+            Console.WriteLine("{0,-30} {1,20:N2} {2,20:N2} {3,20:N2} {4,10:P}",
+                    new string('=', 30),
+                    new string('=', 20),
+                    new string('=', 20),
+                    new string('=', 20),
+                    new string('=', 10));
+            foreach (var item in result)
             {
-                throw new ArgumentException(nome);
-            }
+                var lucro = item.Faturamento - item.Orcamento;
+                var lucroPorcentagem = (item.Faturamento - item.Orcamento) / item.Orcamento;
 
-            return nome == "Aracaju";
+                Console.WriteLine("{0,-30} {1,20:N2} {2,20:N2} {3,20:N2} {4,10:P}",
+                    item.Item,
+                    item.Faturamento,
+                    item.Orcamento,
+                    lucro,
+                    lucroPorcentagem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("FIM DO RELATÓRIO: {0}", tituloRelatorio);
+
+            Console.WriteLine("[Tecle algo para continuar]");
+            Console.ReadLine();
         }
     }
 }
