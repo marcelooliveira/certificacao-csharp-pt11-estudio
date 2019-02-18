b4e9af7393e2c78db882e26b42c4d81db3ac3583
marcelooliveira <mclricardo@gmail.com>
2019-02-18 13:37:11 -0300

    .

--- Program01.06/Program.cs
+++ Program01.06/Program.cs
@@ -11,32 +11,16 @@ namespace Program01._08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("System.Diagnostics.Process.GetCurrentProcess().Threads: {0}", System.Diagnostics.Process.GetCurrentProcess().Threads.Count); ;
-
-            Task[] Tarefas = new Task[10];
-            for (int i = 0; i < 10; i++)
-            {
-                int numeroTarefa = i; // cria uma cópia local do contador de loop, para que o
-                                      // número da tarefa seja passado para a expressão lambda
-
-                Tarefas[i] = Task.Run(() => ExecutarTrabalho(numeroTarefa));
-            }
-
-            Task.WaitAll(Tarefas);
-
-            Console.WriteLine("System.Diagnostics.Process.GetCurrentProcess().Threads: {0}", System.Diagnostics.Process.GetCurrentProcess().Threads.Count); ;
-            Console.ReadLine();
-
             Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
             Console.ReadLine();
         }
 
-        public static void ExecutarTrabalho(int i)
+        public static void Correr(int numeroCorredor)
         {
-            Console.WriteLine("Tarefa {0} iniciando", i);
+            Console.WriteLine("Corredor {0} largou", numeroCorredor);
 
             Thread.Sleep(1000);
-            Console.WriteLine("Tarefa {0} terminada", i);
+            Console.WriteLine("Corredor {0} terminou", numeroCorredor);
         }
     }
 }
