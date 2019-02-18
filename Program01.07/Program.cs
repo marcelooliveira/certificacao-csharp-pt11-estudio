0d6e47c91203d66e37244a0d96f82de481ff4c9a
marcelooliveira <mclricardo@gmail.com>
2019-02-18 14:44:59 -0300

    ;

--- Program01.07/Program.cs
+++ Program01.07/Program.cs
@@ -10,43 +10,9 @@ namespace Program01._09
 {
     class Program
     {
-        public static void OlaTask()
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine("Olá");
-            //throw new Exception();
-        }
-
-        public static void MundoTask()
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine("Mundo");
-        }
-
-        public static void ExceptionTask()
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine("Falha na tarefa!");
-        }
-
         static void Main(string[] args)
         {
-            //Task tarefa = Task.Run(() => OlaTask());
-            //tarefa.ContinueWith((tarefaAnterior) => MundoTask());
-            //Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
 
-            Task task = Task.Run(() => OlaTask());
-            task.ContinueWith((tarefaAnterior) => MundoTask(), TaskContinuationOptions.OnlyOnRanToCompletion);
-            task.ContinueWith((tarefaAnterior) =>
-            { // Get the antecedent's exception information.
-                foreach (var ex in tarefaAnterior.Exception.InnerExceptions)
-                {
-                    if (ex is FileNotFoundException)
-                        Console.WriteLine(ex.Message);
-                }
-            }, TaskContinuationOptions.OnlyOnFaulted);
-
-            Console.ReadLine();
         }
     }
 }
