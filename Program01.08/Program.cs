cd284758f6c11f9465425e7bae9eb351546043fe
marcelooliveira <mclricardo@gmail.com>
2019-02-18 15:30:25 -0300

    .

--- Program01.08/Program.cs
+++ Program01.08/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace Program01._10
+﻿namespace Program01._10
 {
     class Program
     {
