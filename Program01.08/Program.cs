45d75c654fdbb2bc70b86baccae9e1a8a313a078
marcelooliveira <mclricardo@gmail.com>
2019-02-18 15:29:30 -0300

    .

--- Program01.08/Program.cs
+++ Program01.08/Program.cs
@@ -9,29 +9,8 @@ namespace Program01._10
 {
     class Program
     {
-        public static void ExecutarFilha(object state)
-        {
-            Console.WriteLine("\tTarefa-filha {0} inciando", state);
-            Thread.Sleep(2000);
-            Console.WriteLine("\tTarefa-filha {0} terminada", state);
-        }
-
         static void Main(string[] args)
         {
-            var tarefaMae = Task.Factory.StartNew(() => {
-            Console.WriteLine("Tarefa-mãe iniciada");
-                for (int i = 0; i < 10; i++)
-                {
-                    int numeroTarefa = i;
-                    Task.Factory.StartNew(
-                    (x) => ExecutarFilha(x), // expressão lambda
-                    numeroTarefa, // estado
-                    TaskCreationOptions.AttachedToParent);
-                }
-            });
-            tarefaMae.Wait(); // aguarda todas as tarefas-filhas terminarem
-            Console.WriteLine("Tarefa-mãe terminou. Tecle enter para encerrar.");
-            Console.ReadLine();
         }
     }
 }
