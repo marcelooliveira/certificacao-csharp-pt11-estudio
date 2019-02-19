f99ad8c3d35bf401adcea9629501458cc3bf1286
marcelooliveira <mclricardo@gmail.com>
2019-02-19 12:49:29 -0300

    1162-CertificacaoCSharpParte11-video4.3-Obtendo Informações Sobre a Thread.trec

--- Program02/Program.cs
+++ Program02/Program.cs
@@ -7,15 +7,20 @@ namespace Program02
     {
         static void Main(string[] args)
         {
+            Thread.CurrentThread.Name = "A thread principal";
+            ExibirThread(Thread.CurrentThread);
+
             //1. Task X Thread
 
             Thread thread1 = new Thread(Executar);
+            thread1.Name = "1. Task X Thread";
             thread1.Start();
             thread1.Join();
 
             //2. Thread com expressão lambda
 
             Thread thread2 = new Thread(() => Executar());
+            thread2.Name = "2. Thread com expressão lambda";
             thread2.Start();
             thread2.Join();
             //3. Passando parâmetro para thread
@@ -25,6 +30,7 @@ namespace Program02
                 ExecutarComParametro(p));
 
             Thread thread3 = new Thread(ps);
+            thread3.Name = "2. Thread com expressão lambda";
             thread3.Start(123);
             thread3.Join();
             //4. Interrompendo um relógio
@@ -32,6 +38,7 @@ namespace Program02
             bool relogioFuncionando = true;
             Thread thread4 = new Thread(() => 
             {
+                ExibirThread(Thread.CurrentThread);
                 while (relogioFuncionando)
                 {
                     Console.WriteLine("tic");
@@ -40,6 +47,7 @@ namespace Program02
                     Thread.Sleep(1000);
                 }
             });
+            thread4.Name = "4. Interrompendo um relógio";
             thread4.Start();
             
             Console.WriteLine("Tecle algo para interromper.");
@@ -58,6 +66,7 @@ namespace Program02
 
         static void Executar()
         {
+            ExibirThread(Thread.CurrentThread);
             Console.WriteLine("Início da execução");
             Thread.Sleep(1000);
             Console.WriteLine("Fim da execução");
@@ -66,9 +75,23 @@ namespace Program02
 
         static void ExecutarComParametro(object param)
         {
+            ExibirThread(Thread.CurrentThread);
             Console.WriteLine("Início da execução: {0}", param);
             Thread.Sleep(1000);
             Console.WriteLine("Fim da execução: {0}", param);
+        }
+
+        static void ExibirThread(Thread t)
+        {
+            //Dados da Thread: Nome, cultura, prioridade, contexto, background, pool
+            Console.WriteLine();
+            Console.WriteLine("Nome: {0}", t.Name);
+            Console.WriteLine("Cultura: {0}", t.CurrentCulture);
+            Console.WriteLine("Prioridade: {0}", t.Priority);
+            Console.WriteLine("Contexto: {0}", t.ExecutionContext);
+            Console.WriteLine("Está em background? {0}", t.IsBackground);
+            Console.WriteLine("Está no pool de threads? {0}", t.IsThreadPoolThread);
+            Console.WriteLine();
         }
     }
 }
