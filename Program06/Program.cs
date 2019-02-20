4aa3aeea24c19103596aed9caa2c85d463b63c27
marcelooliveira <mclricardo@gmail.com>
2019-02-20 12:56:59 -0300

    .

--- Program06/Program.cs
+++ Program06/Program.cs
@@ -1,13 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Program06
 {
     class Program
     {
+        //// make an array that holds the values 0 to 50000000
+        //static int[] items = Enumerable.Range(0, 50000001).ToArray();
+        //static void Main(string[] args)
+        //{
+        //    long total = 0;
+        //    for (int i = 0; i < items.Length; i++)
+        //        total = total + items[i];
+        //    Console.WriteLine("The total is: {0}", total);
+        //    Console.ReadKey();
+        //    Console.ReadLine();
+        //}
+
+
+
+
+
+        static long sharedTotal;
+        // make an array that holds the values 0 to 5000000
+        static int[] items = Enumerable.Range(0, 500001).ToArray();
+        static void addRangeOfValues(int start, int end)
+        {
+            while (start < end)
+            {
+                sharedTotal = sharedTotal + items[start];
+                start++;
+            }
+        }
+
         static void Main(string[] args)
         {
-            //Sincronizar recursos
-            Console.ReadLine();
+            List<Task> tasks = new List<Task>();
+            int rangeSize = 1000;
+            int rangeStart = 0;
+
+
+            while (rangeStart < items.Length)
+            {
+                int rangeEnd = rangeStart + rangeSize;
+                if (rangeEnd > items.Length)
+                    rangeEnd = items.Length;
+
+                // create local copies of the parameters
+                int rs = rangeStart;
+                int re = rangeEnd;
+                tasks.Add(Task.Run(() => addRangeOfValues(rs, re)));
+                rangeStart = rangeEnd;
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("The total is: {0}", sharedTotal);
+            Console.ReadKey();
         }
     }
 }
--- Program07/Program.cs
+++ Program07/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Program07
 {
     class Program
     {
+        static long sharedTotal;
+        //static object sharedTotalLock = new object();
+        // make an array that holds the values 0 to 5000000
+        static int[] items = Enumerable.Range(0, 500001).ToArray();
+        static void addRangeOfValues(int start, int end)
+        {
+            while (start < end)
+            {
+                //lock(sharedTotalLock)
+                //{
+                    sharedTotal = sharedTotal + items[start];
+                //}
+                start++;
+            }
+        }
+
         static void Main(string[] args)
         {
-            //Implementar bloqueio
+            List<Task> tasks = new List<Task>();
+            int rangeSize = 1000;
+            int rangeStart = 0;
+
+
+            while (rangeStart < items.Length)
+            {
+                int rangeEnd = rangeStart + rangeSize;
+                if (rangeEnd > items.Length)
+                    rangeEnd = items.Length;
+
+                // create local copies of the parameters
+                int rs = rangeStart;
+                int re = rangeEnd;
+                tasks.Add(Task.Run(() => addRangeOfValues(rs, re)));
+                rangeStart = rangeEnd;
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("The total is: {0}", sharedTotal);
             Console.ReadLine();
         }
     }
