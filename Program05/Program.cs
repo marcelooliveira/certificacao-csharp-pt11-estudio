386dc26203673d6ffcd13bce4a1cea5def1fc2a6
marcelooliveira <mclricardo@gmail.com>
2019-02-20 11:25:58 -0300

    .

--- Program05/Program.cs
+++ Program05/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Program05
+namespace Program05_01
 {
+    //Gerenciar dados usando coleções simultâneas
     class Program
     {
         static void Main(string[] args)
         {
-            //Gerenciar dados usando coleções simultâneas
+            //Queue<int> colecao = new Queue<int>(5);
+            BlockingCollection<int> colecao = new BlockingCollection<int>(5);
+
+            var tarefa1 =
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    //colecao.Enqueue(i);
+                    colecao.Add(i);
+                    Console.WriteLine("Item {0} adicionado com sucesso.", i);
+                }
+
+                //Sinaliza que a BlockingCollection
+                //não deve receber mais itens
+                colecao.CompleteAdding();
+            });
+
+            Console.ReadKey();
+            Console.WriteLine("Lendo a coleção...");
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        //Remove um item da BlockingCollection
+                        int v = colecao.Take();
+                        //int v = colecao.Dequeue();
+                        Console.WriteLine("Item {0} removido com sucesso.", v);
+                    }
+                }
+                catch (InvalidOperationException exc)
+                {
+                }
+            });
+
             Console.ReadLine();
         }
     }
