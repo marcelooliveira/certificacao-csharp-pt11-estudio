e4b2bc2dec79c1d60141449efc7f0875ba2b9c63
marcelooliveira <mclricardo@gmail.com>
2019-02-20 15:46:12 -0300

    .

--- Program05/Program.cs
+++ Program05/Program.cs
@@ -9,181 +9,46 @@ namespace Program05_01
     //Gerenciar dados usando coleções simultâneas
     class Program
     {
-        static void Mainxxx(string[] args)
+        static void Main()
         {
-            //Queue<int> colecao = new Queue<int>(5);
-            //BlockingCollection<int> colecao = new BlockingCollection<int>(5);
-            ConcurrentQueue<int> colecao = new ConcurrentQueue<int>();
+            int NUMERO_ITENS = 30;
+            int capacidadeInicial = 39;
 
-            var tarefa1 =
-            Task.Run(() =>
+            Dictionary<int, int> dicionario = new Dictionary<int, int>(capacidadeInicial);
+
+            Console.WriteLine("Inicializando dicionário...");
+            for (int i = 0; i < NUMERO_ITENS; i++)
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    int item = i;
-                    colecao.Enqueue(item);
-                    //colecao.Add(i);
-                    Console.WriteLine("Item {0} adicionado com sucesso.", item);
-                }
+                dicionario[i] = 0;
+            }
+            ImprimirItens(dicionario);
 
-                //Sinaliza que a BlockingCollection
-                //não deve receber mais itens
-                //colecao.CompleteAdding();
-            });
+            Console.WriteLine("Incrementando valores...");
 
-            //Console.ReadKey();
-            Console.WriteLine("Lendo a coleção...");
-
-            Task.Run(() =>
+            Thread task1 = new Thread(() =>
             {
-                try
+                for (int i = 0; i < NUMERO_ITENS; i++)
                 {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        //Remove um item da BlockingCollection
-                        //int v = colecao.Take();
-                        colecao.TryDequeue(out int v);
-                        Console.WriteLine("Item {0} removido com sucesso.", v);
-                    }
-                }
-                catch (InvalidOperationException exc)
-                {
+                    dicionario[i]++;
+                    Thread.Sleep(i);
                 }
             });
+            task1.Start();
 
+            task1.Join();
+
+            ImprimirItens(dicionario);
+
+            Console.WriteLine("Tecle [ENTER] para continuar");
             Console.ReadLine();
         }
 
-        static void Mainyyy()
+        private static void ImprimirItens(IDictionary<int, int> cd)
         {
-            // We know how many items we want to insert into the ConcurrentDictionary.
-            // So set the initial capacity to some prime number above that, to ensure that
-            // the ConcurrentDictionary does not need to be resized while initializing it.
-            int NUMITEMS = 64;
-            int initialCapacity = 101;
-
-            // The higher the concurrencyLevel, the higher the theoretical number of operations
-            // that could be performed concurrently on the ConcurrentDictionary.  However, global
-            // operations like resizing the dictionary take longer as the concurrencyLevel rises. 
-            // For the purposes of this example, we'll compromise at numCores * 2.
-            int numProcs = Environment.ProcessorCount;
-            int concurrencyLevel = numProcs * 2;
-
-            // Construct the dictionary with the desired concurrencyLevel and initialCapacity
-            //ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>(concurrencyLevel, initialCapacity);
-            Dictionary<int, int> cd = new Dictionary<int, int>(initialCapacity);
-
-            // Initialize the dictionary
-            for (int i = 0; i < NUMITEMS; i++) cd[i] = i * i;
-
-            Console.WriteLine("The square of 23 is {0} (should be {1})", cd[23], 23 * 23);
-
-            Console.ReadLine();
-        }
-
-        static void Mainzzz()
-        {
-            // Construct a ConcurrentQueue.
-            ConcurrentQueue<int> cq = new ConcurrentQueue<int>();
-
-            // Populate the queue.
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < cd.Count; i++)
             {
-                cq.Enqueue(i);
+                Console.WriteLine("cd[{0}] = {1}", i, cd[i]);
             }
-
-            // Peek at the first element.
-            int result;
-            if (!cq.TryPeek(out result))
-            {
-                Console.WriteLine("CQ: TryPeek failed when it should have succeeded");
-            }
-            else if (result != 0)
-            {
-                Console.WriteLine("CQ: Expected TryPeek result of 0, got {0}", result);
-            }
-
-            int outerSum = 0;
-            // An action to consume the ConcurrentQueue.
-            Action action = () =>
-            {
-                int localSum = 0;
-                int localValue;
-                while (cq.TryDequeue(out localValue)) localSum += localValue;
-                Interlocked.Add(ref outerSum, localSum);
-            };
-
-            // Start 4 concurrent consuming actions.
-            Parallel.Invoke(action, action, action, action);
-
-            Console.WriteLine("outerSum = {0}, should be 49995000", outerSum);
-            Console.ReadLine();
-
-        }
-
-        static async Task Main()
-        {
-            int items = 10000;
-
-            ConcurrentStack<int> stack = new ConcurrentStack<int>();
-
-            // Create an action to push items onto the stack
-            Action pusher = () =>
-            {
-                for (int i = 0; i < items; i++)
-                {
-                    stack.Push(i);
-                }
-            };
-
-            // Run the action once
-            pusher();
-
-            if (stack.TryPeek(out int result))
-            {
-                Console.WriteLine($"TryPeek() saw {result} on top of the stack.");
-            }
-            else
-            {
-                Console.WriteLine("Could not peek most recently added number.");
-            }
-
-            // Empty the stack
-            stack.Clear();
-
-            if (stack.IsEmpty)
-            {
-                Console.WriteLine("Cleared the stack.");
-            }
-
-            // Create an action to push and pop items
-            Action pushAndPop = () =>
-            {
-                Console.WriteLine($"Task started on {Task.CurrentId}");
-
-                int item;
-                for (int i = 0; i < items; i++)
-                    stack.Push(i);
-                for (int i = 0; i < items; i++)
-                    stack.TryPop(out item);
-
-                Console.WriteLine($"Task ended on {Task.CurrentId}");
-            };
-
-            // Spin up five concurrent tasks of the action
-            var tasks = new Task[5];
-            for (int i = 0; i < tasks.Length; i++)
-                tasks[i] = Task.Factory.StartNew(pushAndPop);
-
-            // Wait for all the tasks to finish up
-            await Task.WhenAll(tasks);
-
-            if (!stack.IsEmpty)
-            {
-                Console.WriteLine("Did not take all the items off the stack");
-            }
-
-            Console.ReadLine();
         }
     }
 }
