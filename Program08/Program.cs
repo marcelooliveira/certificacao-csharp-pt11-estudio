b817d5d91026a44b45a8a520d5f8f0998f24df69
marcelooliveira <mclricardo@gmail.com>
2019-02-21 14:54:22 -0300

    1162-CertificacaoCSharpParte11-video7.1-Cancelando Tarefas.trec

--- Program08/Program.cs
+++ Program08/Program.cs
@@ -7,18 +7,22 @@ namespace Program07_01
     //Cancelar uma tarefa de execução longa
     class Program
     {
+        static CancellationTokenSource cancellationTokenSource
+            = new CancellationTokenSource();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Tecle algo para parar o relógio");
             Task relogio = Task.Run(() => Relogio());
             Console.ReadKey();
             Console.WriteLine("O relógio parou.");
+            cancellationTokenSource.Cancel();
             Console.ReadLine();
         }
 
         static void Relogio()
         {
-            while (true)
+            while (!cancellationTokenSource.IsCancellationRequested)
             {
                 Console.WriteLine("Tic");
                 Thread.Sleep(500);
