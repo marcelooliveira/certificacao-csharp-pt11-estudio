add89fb4dd9146c43efb3a19b0342cc3280495c2
marcelooliveira <mclricardo@gmail.com>
2019-02-21 13:37:48 -0300

    .

--- Program09/Program.cs
+++ Program09/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Threading;
 
 namespace Program08
 {
+    //Implementar métodos thread-safe
     class Program
     {
+        class Contador
+        {
+            public int Numero { get; private set; } = 0;
+
+            public void Incrementar()
+            {
+                Numero++;
+            }
+        }
+
         static void Main(string[] args)
         {
-            //Implementar métodos thread-safe
+            var contador = new Contador();
+
+            Console.WriteLine("contador: {0}", contador.Numero);
+
+            Thread thread1 = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    contador.Incrementar();
+                    Thread.Sleep(i);
+                }
+            });
+            thread1.Start();
+
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    contador.Incrementar();
+                    Thread.Sleep(i);
+                }
+            });
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("contador: {0}", contador.Numero);
+
             Console.ReadLine();
         }
     }
