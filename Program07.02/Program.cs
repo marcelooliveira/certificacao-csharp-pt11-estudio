14ad9bb382f00afc38fa90cc829a2648fe2e73c1
marcelooliveira <mclricardo@gmail.com>
2019-02-21 14:58:14 -0300

    .

--- Program07.02/Program.cs
+++ Program07.02/Program.cs
@@ -7,51 +7,29 @@ namespace Program07._02
     //Lançando uma exceção quando a tarefa é cancelada
     class Program
     {
-        static void Relogio(CancellationToken cancellationToken)
-        {
-            int tiques = 0;
+        static CancellationTokenSource cancellationTokenSource
+            = new CancellationTokenSource();
 
-            while (!cancellationToken.IsCancellationRequested 
-                && tiques < 20)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tecle algo para parar o relógio");
+            Task relogio = Task.Run(() => Relogio());
+            Console.ReadKey();
+            Console.WriteLine("O relógio parou.");
+            cancellationTokenSource.Cancel();
+            Console.ReadLine();
+        }
+
+        static void Relogio()
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
             {
-                tiques++;
                 Console.WriteLine("Tic");
                 Thread.Sleep(500);
                 Console.WriteLine("Tac");
                 Thread.Sleep(500);
             }
-            cancellationToken.ThrowIfCancellationRequested();
         }
 
-        static void Main(string[] args)
-        {
-            CancellationTokenSource cancellationTokenSource
-                = new CancellationTokenSource();
-
-            Task relogio = Task.Run(() => Relogio(cancellationTokenSource.Token));
-
-            Console.WriteLine("Tecle algo para parar o relógio");
-            Console.ReadKey();
-
-            if (relogio.IsCompleted)
-            {
-                Console.WriteLine("Tarefa do relógio foi completada.");
-            }
-            else
-            {
-                try
-                {
-                    cancellationTokenSource.Cancel();
-                    relogio.Wait();
-                }
-                catch (AggregateException ex)
-                {
-                    Console.WriteLine("Relógio parou: {0}", 
-                        ex.InnerExceptions[0].ToString());
-                }
-            }
-
-            Console.ReadLine();
-        }
     }
 }
